53d45427355b336d869d3a1abd56fafd53b59d9a
piotrk32 <piotrkepistyuwm@gmail.com>
2021-03-03 18:02:27 +0100

    commit

---
+++ main.py
@@ -0,0 +1,96 @@
+# #zad1
+# a,b = 5, 4
+# print(a,b)
+# c,d = 4.2, 4.5
+# print(c,d)
+# #zad2
+# from math import *
+# x = int(input("Podaj liczbe:"))
+# y = int(input("Podaj droga liczbe:"))
+# z = input("Podaj rodzaj dzialania (+,-,*,/,**): ")
+# dzialania = {"+": x+y, "-": x-y, "*": x*y, "/": x/y, "**": x**y}
+# if z == '+':
+#     print(dzialania["+"])
+# elif z =='-':
+#     print(dzialania ["-"])
+# elif z =='*':
+#     print(dzialania ["*"])
+# elif z =='/':
+#     print(dzialania ["/"])
+# elif z =='**':
+#     print(dzialania ["**"])
+# elif z =='%':
+#     print(dzialania ["%"])
+
+# #zad3
+# a=5;
+# a+=2;
+# print(a)
+# b=5;
+# b-=4;
+# print(b)
+# c=5;
+# c*=4;
+# print(c)
+# d=5;
+# d/=4;
+# print(d)
+# e=5;
+# e**=4;
+# print(e)
+# f=5;
+# f%=4;
+# print(b)
+
+# #zad4
+# e = int(input("Podaj e "))
+# print(e**10)
+
+# from math import *
+# x=(log(5 + sin(8)**2))
+# c=pow(x,1/6)
+# print(c)
+
+
+# x=[3,55]
+# print(min(x))
+# y= [4,80]
+# print(max(y))
+
+# #zad5
+
+# a= "PIOTR"
+# b= "KEPISTY"
+#
+# c = a.capitalize()
+# print(c)
+# d= b.capitalize()
+# print(d)
+
+# zad 6
+#
+piosenka = "Jestem sobie lala i lalala pisze piosenke lalalala"
+la= piosenka.count("la")
+# print(la)
+
+# zad 7
+
+# zmienna = "KOCHAMPIS"
+# index1 = zmienna[2]
+# print(index1)
+# index2 = zmienna[8]
+# print(index2)
+
+# zad 8
+ print(piosenka.split())
+
+# zad 9
+
+s = "MIKI"
+f = 1.23
+print(s)
+print(f)
+n = 1234
+print(n, "in hex= " , hex(n))
+
+
