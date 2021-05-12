9ceecd794db5891c8af8ede66456ff0292698587
piotrk32 <73549369+piotrk32@users.noreply.github.com>
2021-05-12 21:00:32 +0200

    matplothlibzad2
    matplothlibzad2

---
+++ mathplotlibPDzad1,2.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#zad1
+# def f(x):
+#     return 1/ x
+#
+# x = list(range(1,21))
+# y = [f(z) for z in x]
+#
+# f, ax = plt.subplots()
+#
+# ax.plot(x,y,label=('f(x)= 1/x'))
+#
+# ax.legend()
+# ax.set_xlabel('x')
+# ax.set_ylabel('f(x)')
+# ax.set_xlim(1, len(x))
+# ax.set_ylim(0,1)
+# plt.show()
+#zad2
+# def f(x):
+#     return 1/ x
+#
+# x = list(range(1,21))
+# y = [f(z) for z in x]
+#
+# f, ax = plt.subplots()
+#
+# ax.plot(x,y, c='green', label=('f(x)= 1/x'), marker = '^', linestyle = '--')
+#
+# ax.legend()
+# ax.set_xlabel('x')
+# ax.set_ylabel('f(x)')
+# ax.set_xlim(1, len(x))
+# ax.set_ylim(0,1)
+# plt.show()
