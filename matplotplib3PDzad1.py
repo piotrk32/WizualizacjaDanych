749a04478337c55659b8593ab38081eb987f1fd0
piotrk32 <73549369+piotrk32@users.noreply.github.com>
2021-05-19 20:13:50 +0200

    matplotlib3PDzad 1
    matplotlib3PDzad 1

---
+++ matplotplib3PDzad1.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d.axes3d import get_test_data
+
+
+
+
+#zad1
+# z = np.arange(0, np.pi*2, .1)
+# plt.plot(z, np.sin(z), 'b', label='sin(z)')
+# plt.plot(z, np.cos(z)*2, 'r', label='2cos(z)')
+# plt.xlabel('z')
+# plt.ylabel('sin(z) cos(z)')
+# plt.legend(loc='upper right')
+# plt.title('Wykres sin(z), 2cos(z)')
+# plt.show()
